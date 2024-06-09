3d561ac73fd99cfeaae6215465c69f1465ffa9d9
PedroItaloQ <peedroitalo2906@gmail.com>
2024-06-09 15:55:56 -0300

    fix: app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-import express from "express";
-import conectaNaDatabase from "./config/dbConnect.js";
-import routes from "./routes/index.js";
-
-const conexao = await conectaNaDatabase();
-
-conexao.on("error", (erro) => {
-  console.error("erro de conexão", erro);
-});
-
-conexao.once("open", () => {
-  console.log("Conexao com o banco feita com sucesso");
-})
+import express from 'express';
+import conectaNaDatabase from './config/dbConnect.js';
+import routes from './routes/index.js';
 
 const app = express();
+
+conectaNaDatabase().then(() => {
+  console.log('Conexão com o banco feita com sucesso');
+}).catch((erro) => {
+  console.error('Erro ao conectar ao banco', erro);
+});
+
 routes(app);
 
-app.delete("/carros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros.splice(index, 1);
-  res.status(200).send("carro removido com sucesso");
+app.delete('/cars/:id', (req, res) => {
+  const index = buscaCar(req.params.id);
+  if (index !== -1) {
+    cars.splice(index, 1);
+    res.status(200).send('Carro removido com sucesso');
+  } else {
+    res.status(404).send('Carro não encontrado');
+  }
 });
 
 export default app;
